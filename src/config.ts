522019080a3590c125c5d9ead89248c17f9117c2
Zemphis <fardeen.15.l1f3@gmail.com>
2025-11-26 01:17:47 +1030

    folders tweaked

---
+++ src/config.ts
@@ -0,0 +1,92 @@
+export const siteConfig = {
+  name: "Fardeen Rahman",
+  title: "Bachelor of Computer Science",
+  description: "Portfolio website of Fardeen Rahman",
+  accentColor: "#1f39829b",
+  social: {
+    email: "fardeen.mm.rahman@gmail.com",
+    linkedin: "https://linkedin.com/in/fardeen-rahman-9033a8307/",
+    twitter: "https://x.com/rfitzio",
+    github: "https://github.com/fardeenRahman",
+  },
+  aboutMe:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
+  skills: ["Javascript", "React", "Node.js", "C++", "AWS", "Docker", "SQL", "FPGAs", "Verilog", "VHDL"],
+  projects: [
+    {
+      name: "Adelaide University Space Society Website",
+      description:
+        "The official website for the Adelaide University Space Society, allowing people to sign up for a membership and view upcoming events.",
+      link: "https://www.auspacesociety.com.au/",
+      skills: ["React", "Node.js", "Stripe", "Vercel"],
+    },
+    {
+      name: "Chrome Extension Mastery: Build Full-Stack Extensions with React & Node.js",
+      description:
+        "Master the art of building production-ready, full-stack Chrome Extensions using modern web technologies and best practices",
+      link: "https://fullstackextensions.com/?ref=devportfolio",
+      skills: ["React", "Node.js", "AWS"],
+    },
+    {
+      name: "ExtensionKit",
+      description:
+        "Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates & examples",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["React", "Node.js", "AWS"],
+    },
+  ],
+  experience: [
+    {
+      company: "Tech Company",
+      title: "Senior Software Engineer",
+      dateRange: "Jan 2022 - Present",
+      bullets: [
+        "Led development of microservices architecture serving 1M+ users",
+        "Reduced API response times by 40% through optimization",
+        "Mentored team of 5 junior developers",
+      ],
+    },
+    {
+      company: "Startup Inc",
+      title: "Full Stack Developer",
+      dateRange: "Jun 2020 - Dec 2021",
+      bullets: [
+        "Built and launched MVP product from scratch using React and Node.js",
+        "Implemented CI/CD pipeline reducing deployment time by 60%",
+        "Collaborated with product team to define technical requirements",
+      ],
+    },
+    {
+      company: "Digital Agency",
+      title: "Frontend Developer",
+      dateRange: "Aug 2018 - May 2020",
+      bullets: [
+        "Developed responsive web applications for 20+ clients",
+        "Improved site performance scores by 35% on average",
+        "Introduced modern JavaScript frameworks to legacy codebases",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "University Name",
+      degree: "Bachelor of Science in Computer Science",
+      dateRange: "2014 - 2018",
+      achievements: [
+        "Graduated Magna Cum Laude with 3.8 GPA",
+        "Dean's List all semesters",
+        "President of Computer Science Club",
+      ],
+    },
+    {
+      school: "Online Platform",
+      degree: "Full Stack Development Certificate",
+      dateRange: "2019",
+      achievements: [
+        "Completed 500+ hours of coursework",
+        "Built 10+ portfolio projects",
+        "Specialized in React and Node.js",
+      ],
+    },
+  ],
+};
